3ef1d558c396e6a0e85d9971e66ede775d53c203
Fredo Guan <fredo.guan@hotmail.com>
2022-12-10 01:59:03 -0800

    Update davit.py

--- timm/models/davit.py
+++ timm/models/davit.py
@@ -505,7 +505,7 @@ class DaViT(nn.Module):
             stage_drop_rates = dpr[len(attention_types) * sum(depths[:stage_id]):len(attention_types) * sum(depths[:stage_id + 1])]
             
             in_chs = in_chans if stage_id == 0 else embed_dims[stage_id - 1]
-            
+            print(in_chs)
             stage = DaViTStage(
                 in_chs,
                 embed_dims[stage_id],
