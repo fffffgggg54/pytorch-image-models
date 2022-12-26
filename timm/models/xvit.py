4089efc4c5eb236161aaf342511f194ee2bda387
Fredo Guan <fredo.guan@hotmail.com>
2022-12-26 01:11:23 -0800

    Update xvit.py

--- timm/models/xvit.py
+++ timm/models/xvit.py
@@ -60,7 +60,7 @@ class XNormAttention(nn.Module):
         print(q.shape)
         print(k.shape)
         print(v.shape)
-        attn = self.xnorm(v @ k.transpose(-2, -1))
+        attn = self.xnorm(k.transpose(-2, -1) @ v)
         print(attn.shape)
         x = (self.xnorm(q) @ attn).transpose(1, 2).reshape(B, N, C)
         x = self.proj(x)
