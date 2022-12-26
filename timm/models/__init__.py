7b9c10dedc3fa2c4861cb4814284f279fa89aaee
Fredo Guan <fredo.guan@hotmail.com>
2022-12-26 00:56:56 -0800

    Update __init__.py

--- timm/models/__init__.py
+++ timm/models/__init__.py
@@ -63,6 +63,7 @@ from .vovnet import *
 from .xception import *
 from .xception_aligned import *
 from .xcit import *
+from .xvit import *
 
 from ._builder import build_model_with_cfg, load_pretrained, load_custom_pretrained, resolve_pretrained_cfg, \
     set_pretrained_download_progress, set_pretrained_check_hash
