881e961a7760e5fa14768e95457f085dbc5b14d8
Fredo Guan <fredo.guan@hotmail.com>
2024-01-25 15:41:54 -0700

    Update __init__.py

--- timm/layers/__init__.py
+++ timm/layers/__init__.py
@@ -4,7 +4,7 @@ from .adaptive_avgmax_pool import \
 from .attention_pool import AttentionPoolLatent
 from .attention_pool2d import AttentionPool2d, RotAttentionPool2d, RotaryEmbedding
 from .blur_pool import BlurPool2d
-from .classifier import ClassifierHead, create_classifier, NormMlpClassifierHead
+from .classifier import ClassifierHead, create_classifier, NormMlpClassifierHead, PyramidFeatureAggregationModel
 from .cond_conv2d import CondConv2d, get_condconv_initializer
 from .config import is_exportable, is_scriptable, is_no_jit, use_fused_attn, \
     set_exportable, set_scriptable, set_no_jit, set_layer_config, set_fused_attn
