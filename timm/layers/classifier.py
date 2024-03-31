476f350d7714125e585d0467696b3c8ba0c4934d
Fredo Guan <fredo.guan@hotmail.com>
2024-03-31 07:17:54 -0700

    Update classifier.py

--- timm/layers/classifier.py
+++ timm/layers/classifier.py
@@ -24,6 +24,7 @@ class SequencePool(nn.Module):
         self.feature_source = feature_source
         self.input_fmt = None
         self.prefix_len = prefix_len
+        self.global_pool
         
     def forward(self, x):
         if self.pool_type = 'cls':
