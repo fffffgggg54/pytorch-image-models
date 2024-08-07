b8bda4c08529fe36fcc26e05b6a4dfd3e931aae0
Fredo Guan <fredo.guan@hotmail.com>
2024-08-07 01:13:05 -0700

    Update vision_transformer.py

--- timm/models/vision_transformer.py
+++ timm/models/vision_transformer.py
@@ -2301,6 +2301,17 @@ def vit_base_patch16_gap_224(pretrained: bool = False, **kwargs) -> VisionTransf
 
 
 @register_model
+def vit_medium_shallow_patch16_gap_224(pretrained: bool = False, **kwargs) -> VisionTransformer:
+    """ ViT-Base (ViT-B/16) w/o class token, w/ avg-pool @ 224x224
+    """
+    model_args = dict(
+        patch_size=16, embed_dim=512, depth=8, num_heads=8, class_token=False, global_pool='avg', fc_norm=False)
+    model = _create_vision_transformer(
+        'vit_base_patch16_gap_224', pretrained=pretrained, **dict(model_args, **kwargs))
+    return model
+    
+    
+@register_model
 def vit_huge_patch14_gap_224(pretrained: bool = False, **kwargs) -> VisionTransformer:
     """ ViT-Huge model (ViT-H/14) w/ no class token, avg pool
     """
