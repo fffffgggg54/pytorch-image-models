006fbb126828be0f39d635bafc0c241a43357069
Fredo Guan <fredo.guan@hotmail.com>
2025-06-02 21:05:27 -0700

    Masking

--- timm/models/vision_transformer_register_cross_attn.py
+++ timm/models/vision_transformer_register_cross_attn.py
@@ -73,6 +73,58 @@ class CrossAttention(nn.Module):
         x = self.proj_drop(x)
         return x
 
+class MaskingAttention(nn.Module):
+    fused_attn: Final[bool]
+
+    def __init__(
+            self,
+            dim: int,
+            num_heads: int = 8,
+            qkv_bias: bool = True,
+            qk_norm: bool = False,
+            proj_bias: bool = True,
+            attn_drop: float = 0.1,
+            proj_drop: float = 0.1,
+            norm_layer: nn.Module = nn.LayerNorm,
+    ) -> None:
+        super().__init__()
+        assert dim % num_heads == 0, 'dim should be divisible by num_heads'
+        self.num_heads = num_heads
+        self.head_dim = dim // num_heads
+        self.scale = self.head_dim ** -0.5
+        self.fused_attn = use_fused_attn()
+        
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.q_norm = norm_layer(self.head_dim) if qk_norm else nn.Identity()
+        self.k_norm = norm_layer(self.head_dim) if qk_norm else nn.Identity()
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim, bias=proj_bias)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x: torch.Tensor, mask: torch.Tensor = None) -> torch.Tensor:
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv.unbind(0)
+        q, k = self.q_norm(q), self.k_norm(k)
+
+        if self.fused_attn:
+            x = F.scaled_dot_product_attention(
+                q, k, v,
+                dropout_p=self.attn_drop.p if self.training else 0.,
+                attn_mask = mask,
+            )
+        else:
+            q = q * self.scale
+            attn = q @ k.transpose(-2, -1)
+            attn = attn.softmax(dim=-1)
+            attn = self.attn_drop(attn)
+            x = attn @ v
+
+        x = x.transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
 class LayerScale(nn.Module):
     def __init__(
             self,
@@ -308,6 +360,76 @@ class RegisterCrossAttentionBlockV2(nn.Module):
         x = x.transpose(1, 2).reshape(B, C, H, W)
         return (x, registers)
 
+
+class MaskedRegisterAttentionBlock(nn.Module):
+    def __init__(
+            self,
+            dim: int,
+            num_heads: int,
+            mlp_ratio: float = 4.,
+            qkv_bias: bool = True,
+            qk_norm: bool = False,
+            proj_bias: bool = True,
+            attn_drop: float = 0.,
+            proj_drop: float = 0.,
+            init_values: Optional[float] = None,
+            drop_path: float = 0.,
+            act_layer: nn.Module = nn.GELU,
+            norm_layer: nn.Module = nn.LayerNorm,
+            mlp_layer: nn.Module = Mlp,
+    ) -> None:
+        super().__init__()
+
+        self.norm1 = norm_layer(dim)
+        self.token_mixer = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim, bias=True)  # depthwise conv
+        self.ls1 = LayerScale(dim, init_values=init_values) if init_values else nn.Identity()
+        self.drop_path1 = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+        self.norm2 = norm_layer(dim)
+        self.attn = MaskingAttention(
+            dim,
+            num_heads=num_heads,
+            qkv_bias=qkv_bias,
+            qk_norm=qk_norm,
+            proj_bias=proj_bias,
+            attn_drop=attn_drop,
+            proj_drop=proj_drop,
+            norm_layer=norm_layer,
+        )
+        self.ls2 = LayerScale(dim, init_values=init_values) if init_values else nn.Identity()
+        self.drop_path2 = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+        self.norm3 = norm_layer(dim)
+        self.mlp = mlp_layer(
+            in_features=dim,
+            hidden_features=int(dim * mlp_ratio),
+            act_layer=act_layer,
+            bias=proj_bias,
+            drop=proj_drop,
+        )
+        self.ls3 = LayerScale(dim, init_values=init_values) if init_values else nn.Identity()
+        self.drop_path3 = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        
+    def forward(self, in_tuple: Tuple[torch.Tensor, torch.Tensor]) -> Tuple[torch.Tensor, torch.Tensor]:
+        x, registers = in_tuple
+        B, C, H, W = x.shape
+        _, K, _ = registers.shape
+        x = x.flatten(2).transpose(1, 2) # BCHW -> BNC
+        x = x + self.drop_path1(self.ls1(self.token_mixer(self.norm1(x).transpose(1, 2).reshape(B, C, H, W))))
+        x = torch.cat((registers, x), dim=1)
+        mask = torch.ones(K+H*W, K+H*W, dtype=torch.bool, device = x.device, requires_grad = False)
+        mask[K:K+H*W, K:K+H*W] = False
+        x = x + self.drop_path2(self.ls2(self.attn(self.norm2(x), mask=mask)))
+        x = x + self.drop_path3(self.ls3(self.mlp(self.norm3(x))))
+
+        
+        registers = x[:, :K]
+        x = x[:, K:]
+        # BNC -> BCHW
+        x = x.transpose(1, 2).reshape(B, C, H, W)
+        return (x, registers)
+
+
 class RegisterCrossAttentionViT(VisionTransformer):
     def __init__(
         self,
@@ -457,17 +579,23 @@ def vit_base_patch16_reg4_ca_224(pretrained: bool = False, **kwargs) -> Register
 @register_model
 def vit_base_patch16_reg4_cav2_224(pretrained: bool = False, **kwargs) -> RegisterCrossAttentionViT:
     model_args = dict(patch_size=16, embed_dim=768, depth=12, num_heads=12, block_fn=RegisterCrossAttentionBlockV2)
-    model = _create_registercrossattentionvit('vit_base_patch16_reg4_ca_224', pretrained=pretrained, **dict(model_args, **kwargs))
+    model = _create_registercrossattentionvit('vit_base_patch16_reg4_cav2_224', pretrained=pretrained, **dict(model_args, **kwargs))
     return model
     
 @register_model
 def vit_base_patch16_reg64var_ca_224(pretrained: bool = False, **kwargs) -> RegisterCrossAttentionViT:
     model_args = dict(patch_size=16, embed_dim=768, depth=12, num_heads=12, reg_tokens=64, variable_reg_token_count=True)
-    model = _create_registercrossattentionvit('vit_base_patch16_reg4_ca_224', pretrained=pretrained, **dict(model_args, **kwargs))
+    model = _create_registercrossattentionvit('vit_base_patch16_reg64var_ca_224', pretrained=pretrained, **dict(model_args, **kwargs))
     return model
     
 @register_model
 def vit_base_patch16_reg64var_cav2_224(pretrained: bool = False, **kwargs) -> RegisterCrossAttentionViT:
     model_args = dict(patch_size=16, embed_dim=768, depth=12, num_heads=12, reg_tokens=64, variable_reg_token_count=True, block_fn=RegisterCrossAttentionBlockV2)
-    model = _create_registercrossattentionvit('vit_base_patch16_reg4_ca_224', pretrained=pretrained, **dict(model_args, **kwargs))
+    model = _create_registercrossattentionvit('vit_base_patch16_reg64var_cav2_224', pretrained=pretrained, **dict(model_args, **kwargs))
+    return model
+
+@register_model
+def vit_base_patch16_reg16_masking_224(pretrained: bool = False, **kwargs) -> RegisterCrossAttentionViT:
+    model_args = dict(patch_size=16, embed_dim=768, depth=12, num_heads=12, reg_tokens=16, variable_reg_token_count=True, block_fn=MaskedRegisterAttentionBlock)
+    model = _create_registercrossattentionvit('vit_base_patch16_reg16_masking_224', pretrained=pretrained, **dict(model_args, **kwargs))
     return model
